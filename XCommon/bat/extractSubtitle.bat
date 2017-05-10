79dcf2c288cf772e514e4f69f560c6d4d2875f8a
xie <xiehp@users.noreply.github.com>
2017-05-10 08:34:57 +0800

    抽取字幕的bat

--- XCommon/bat/extractSubtitle.bat
+++ XCommon/bat/extractSubtitle.bat
@@ -1,8 +1,17 @@
 
 set /p videoPath=
 
-cd %videoPath%
+echo %cd%
+echo %~dp0
+
+set oldPath=%cd%
+
+cd /d %videoPath%
 
 for %%c in (*.mkv) do mkvextract tracks "%%c" 2:"%%c_track3.ass" 3:"%%c_track4.ass" 4:"%%c_track5.ass"
+
+cd /d %oldPath%
+
+rem 运行结束
 
 pause
