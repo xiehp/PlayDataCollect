f6112fd5466b517b013d57fb6386734a2561edb6
xie <xiehp@users.noreply.github.com>
2017-12-10 21:39:49 +0800

    111

--- PlayDataCollect-Collector/src/main/java/xie/playdatacollect/collector/MainCollector.java
+++ PlayDataCollect-Collector/src/main/java/xie/playdatacollect/collector/MainCollector.java
@@ -16,6 +16,9 @@ public class MainCollector {
     }
 
     public static void main(String[] args) throws Exception {
+        // 完全不使用开发辅助工具热重启
+        //System.setProperty("spring.devtools.restart.enabled", "false");
+
         SpringApplication.run(MainCollector.class, args);
     }
 }
