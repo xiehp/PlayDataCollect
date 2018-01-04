7903d09af42df8103f7720ee0824788238e1ab85
xie <xie@qq.com>
2018-01-04 13:57:15 +0800

    study

---
+++ dummy/playdatacollect-dummy-server/src/main/java/xie/playdatacollect/server/MainDummyServer.java
@@ -0,0 +1,47 @@
+package xie.playdatacollect.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@RestController
+@EnableAutoConfiguration
+public class MainDummyServer {
+
+	@RequestMapping("/")
+	public String index() {
+		return "This is Dummy Server!";
+	}
+
+
+	int site1_getPayCount_request_count = 0;
+	Random random = new Random(100);
+	@RequestMapping("/site1/getPayCount")
+	@ResponseBody
+	public Map<String, Object> site1_getPayCount(
+			@RequestParam(required = false) String title) {
+		int aaa = random.nextInt(10) + site1_getPayCount_request_count;
+		site1_getPayCount_request_count = aaa;
+
+		Map<String, Object> getPayCount = new HashMap<>();
+		getPayCount.put("title", StringUtils.isEmpty(title) ? "the title" : title);
+		getPayCount.put("payCount", aaa);
+		return getPayCount;
+	}
+
+	public static void main(String[] args) {
+		// 完全不使用开发辅助工具热重启
+		//System.setProperty("spring.devtools.restart.enabled", "false");
+
+		SpringApplication.run(MainDummyServer.class, args);
+	}
+}
--- playdatacollect-monitor/src/main/java/xie/playdatacollect/server/MainMonitor.java
+++ playdatacollect-monitor/src/main/java/xie/playdatacollect/server/MainMonitor.java
@@ -2,7 +2,6 @@ package xie.playdatacollect.server;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,10 +13,10 @@ public class MainMonitor {
 		return "This is Server!";
 	}
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		// 完全不使用开发辅助工具热重启
 		//System.setProperty("spring.devtools.restart.enabled", "false");
 
-		SpringApplication.run(MainServer.class, args);
+		SpringApplication.run(MainMonitor.class, args);
 	}
 }
