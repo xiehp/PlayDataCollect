29051fbd59655898e83f61eb939d427214dbba8a
xie <xiehp@users.noreply.github.com>
2018-02-05 00:53:16 +0800

    学习

--- app/playdatacollect-collector/src/main/java/xie/playdatacollect/collector/MainCollector.java
+++ app/playdatacollect-collector/src/main/java/xie/playdatacollect/collector/MainCollector.java
@@ -1,5 +1,10 @@
 package xie.playdatacollect.collector;
 
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Point;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -10,7 +15,16 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import us.codecraft.webmagic.ResultItems;
+import us.codecraft.webmagic.Spider;
 import xie.module.httpclient.XHttpClientUtils;
+import xie.playdatacollect.core.entity.MetricEntity;
+import xie.playdatacollect.core.entity.TagEntity;
+import xie.playdatacollect.core.entity.ValueEntity;
+import xie.playdatacollect.core.service.MetricService;
+import xie.playdatacollect.core.service.TagService;
+import xie.playdatacollect.core.service.ValueService;
+import xie.playdatacollect.spider.webmagic.study2.BilibiliAnimePageProcessor;
 import xie.playdatacollect.testandstudy.db.app.fun.test1.Test1.Test1Entity;
 import xie.playdatacollect.testandstudy.db.app.fun.test1.Test1.Test1Service;
 import xie.playdatacollect.testandstudy.db.app.fun.test1.Test2.Test2Entity;
@@ -18,10 +32,8 @@ import xie.playdatacollect.testandstudy.db.app.fun.test1.Test2.Test2Service;
 
 import javax.annotation.Resource;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @RestController
@@ -39,6 +51,12 @@ public class MainCollector {
 	Test1Service test1Service;
 	@Resource
 	Test2Service test2Service;
+	@Resource
+	TagService tagService;
+	@Resource
+	MetricService metricService;
+	@Resource
+	ValueService valueService;
 
 	@Bean
 	public RestTemplate createRestTemplate() {
@@ -111,9 +129,98 @@ public class MainCollector {
 	/**
 	 * 从0点开始,每2个小时执行一次
 	 */
-	@Scheduled(cron = "0 0 0 1 * ? ")
+	@Scheduled(cron = "0/30 * * * * ?")
 	public void runScheduled() {
-		System.out.println("----开始执行简书定时任务");
+		System.out.println(new Date() + "----开始执行定时抓取任务");
+
+		try {
+
+			tagService.insertNewKeyName("bilibili", "bilibili", TagEntity.class);
+			tagService.insertNewKeyName("bilibili" + new Date(), "bilibili" + new Date(), TagEntity.class);
+			tagService.insertNewKeyName("youku", "优酷", TagEntity.class);
+			tagService.insertNewKeyName("iqiyi", "爱奇艺", TagEntity.class);
+
+			metricService.insertNewKeyName("play", "播放量", MetricEntity.class);
+			metricService.insertNewKeyName("fans", "追番量", MetricEntity.class);
+			metricService.insertNewKeyName("review", "弹幕量", MetricEntity.class);
+			metricService.insertNewKeyName(new Date() + "", new Date() + "", MetricEntity.class);
+
+			System.out.println(metricService.findByKey("review"));
+			System.out.println("MyCommandLineRunner1 end");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+		runSpider();
+	}
+
+	private void runSpider() {
+		Spider spider = Spider.create(new BilibiliAnimePageProcessor()).thread(1);
+
+		// multi download
+		List<String> list = new ArrayList<>();
+
+		list.add("https://www.bilibili.com/bangumi/play/ep173286"); // 紫罗兰剧集2
+		list.add("https://bangumi.bilibili.com/anime/21542"); // 紫罗兰动画
+		list.add("https://bangumi.bilibili.com/anime/6445"); // 3月的狮子 第二季
+		list.add("https://www.bilibili.com/bangumi/play/ep115339"); // 3月的狮子 第二季 第24话 混沌/隈仓
+
+		long dateTime = System.currentTimeMillis();
+		Logger log = LoggerFactory.getLogger(BilibiliAnimePageProcessor.class);
+		Logger logSpider = LoggerFactory.getLogger(Spider.class);
+		List<ResultItems> resultItemses = spider.getAll(list);
+		InfluxDB influxDB = InfluxDBFactory.connect("http://172.17.0.2:8086", "root", "root");
+		while (true) {
+			for (ResultItems resultItemse : resultItemses) {
+				log.info(resultItemse.getAll().toString());
+
+				String 名字 = resultItemse.getAll().get("名字").toString();
+				int 播放数 = 0;
+				int 追番人数 = 0;
+				int 弹幕总数 = 0;
+				resultItemse.getAll().forEach((key, value) -> {
+					ValueEntity valueEntity = new ValueEntity();
+					valueEntity.setTag(key);
+					valueEntity.setTime(new Date());
+					valueEntity.setValue(value == null ? null : value.toString());
+					valueEntity = valueService.save(valueEntity);
+					//System.out.println(valueEntity.toMapWithOutBase());
+
+
+					if ("播放数".equals(key) || "追番人数".equals(key) || "弹幕总数".equals(key)) {
+						try {
+							if (value != null && value.toString().contains("万")) {
+								value = value.toString().replace("万", "");
+								value = (int)(Double.valueOf(value.toString()) * 10000);
+							}
+							//String requestObject = "cpu_load_short,host=server01,region=us-west value=0.64 1434055562000000000";
+							String requestObject = key + ",名字=" + 名字 + ",网站=bilibili value=" + value + " " + dateTime + "";
+							String result = restTemplate.postForObject("http://linux2.acgimage.cn:48086/write?db=play_data", requestObject, String.class);
+
+							influxDB.write(Point.measurement(key)
+									.time(dateTime, TimeUnit.MILLISECONDS)
+									.addField("网站", "bilibili")
+									.addField("名字", 名字)
+									.addField("播放数", 播放数)
+									.addField("追番人数", 追番人数)
+									.addField("弹幕总数", 弹幕总数)
+									.build());
+							influxDB.write("cpu,atag=test1 idle=100,usertime=10,system=1");
+
+							log.info(result);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+
+			}
+
+			break;
+		}
+		influxDB.close();
+		spider.close();
 	}
 
 	public static void main(String[] args) {
--- component/spider/playdatacollect-spider-webmagic/src/main/java/xie/playdatacollect/spider/webmagic/study2/BilibiliAnimePageProcessor.java
+++ component/spider/playdatacollect-spider-webmagic/src/main/java/xie/playdatacollect/spider/webmagic/study2/BilibiliAnimePageProcessor.java
@@ -12,12 +12,24 @@ public class BilibiliAnimePageProcessor implements PageProcessor {
 	public void process(Page page) {
 		String name = page.getHtml().$(".info-content .b-head h1", "title").get();
 		page.putField("名字", name);
-		if (page.getResultItems().getAll().get("名字") == null) {
+		if (page.getResultItems().getAll().get("名字") == null || "null".equals(page.getResultItems().getAll().get("名字"))) {
 			page.putField("名字", page.getHtml().$(".header-info h1", "text"));
 		}
+
 		page.putField("播放数", page.getHtml().$(".info-count-item-play em", "text"));
+		if (page.getResultItems().getAll().get("播放数") == null || page.getResultItems().getAll().get("播放数").toString() == null) {
+			page.putField("播放数", page.getHtml().$(".view-count span", "text"));
+		}
+
 		page.putField("追番人数", page.getHtml().$(".info-count-item-fans em", "allText"));
+		if (page.getResultItems().getAll().get("追番人数") == null || page.getResultItems().getAll().get("追番人数").toString() == null) {
+			page.putField("追番人数", page.getHtml().$(".bangumi-order-wrap #v_ctimes", "text"));
+		}
+
 		page.putField("弹幕总数", page.getHtml().$(".info-count-item-review em", "text"));
+		if (page.getResultItems().getAll().get("弹幕总数") == null || page.getResultItems().getAll().get("弹幕总数").toString() == null) {
+			page.putField("弹幕总数", page.getHtml().$(".count-wrapper .danmu-count span", "text"));
+		}
 //		page.putField("description", page.getHtml().xpath("//div[@class='lemma-summary']/allText()"));
 	}
 
