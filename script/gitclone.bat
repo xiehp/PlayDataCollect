c76dff4746c1f8d7a5b21d1b9a4fad35c4cd0941
xie <xiehp@users.noreply.github.com>
2018-02-18 23:41:13 +0800

    学习

---
+++ script/gitclone.bat
@@ -0,0 +1,10 @@
+git clone --recursive https://github.com/xiehp/PlayDataCollect.git
+
+::git clone https://github.com/xiehp/PlayDataCollect.git
+::cd PlayDataCollect
+::git submodule init
+::git submodule update
+::cd common
+::git checkout SplitProject
+
+pause
