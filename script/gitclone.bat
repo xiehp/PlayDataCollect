2c423656870560fef5ad0ba02363442de100edec
xie <xiehp@users.noreply.github.com>
2018-02-19 00:23:35 +0800

    学习

--- script/gitclone.bat
+++ script/gitclone.bat
@@ -1,4 +1,6 @@
 git clone --recursive https://github.com/xiehp/PlayDataCollect.git
+cd PlayDataCollect
+mvn clean package
 
 ::git clone https://github.com/xiehp/PlayDataCollect.git
 ::cd PlayDataCollect
