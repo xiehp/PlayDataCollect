b9d24aac564590d3d9bcbe0ff3454f74866a0a12
xie <xiehp@users.noreply.github.com>
2018-03-29 00:13:39 +0800

    学习

---
+++ dockerBuild.sh
@@ -0,0 +1,8 @@
+
+echo export JAVA_HOME
+java -version
+mvn --version
+
+docker build -t xie/playdata --no-cache .
+
+pause
--- script/docker/aliyun/dockerBuild.sh
+++ script/docker/aliyun/dockerBuild.sh
@@ -1,24 +1,32 @@
 
-echo export JAVA_HOME
+echo ${JAVA_HOME}
+echo
 java -version
+echo
+mvn --version
+echo
+ls -l
+echo
 
-git clone --recursive https://github.com/xiehp/PlayDataCollect.git
-cd PlayDataCollect
+bash script/docker/aliyun/dockerBuild.sh
+
+# aliyun不会自动下载子项目
+git submodule init
 git submodule update
-mvn clean package
+mvn package -Dmaven.test.skip=true
 
 
-echo 复制文件 $PWD/playdatacollect/app/playdatacollect-collector/target/playdatacollect-collector.jar
-cp $PWD/app/playdatacollect-collector/target/playdatacollect-collector.jar ../playdatacollect-collector.jar
+echo copy $PWD/playdatacollect/app/playdatacollect-collector/target/playdatacollect-collector.jar
+cp $PWD/app/playdatacollect-collector/target/playdatacollect-collector.jar playdatacollect-collector.jar
+ls -l
+echo
 
-echo 回到先前目录
+echo back to parent dir
 cd ..
+ls -l
+echo
 
-::git clone https://github.com/xiehp/PlayDataCollect.git
-::cd PlayDataCollect
-::git submodule init
-::git submodule update
-::cd common
-::git checkout SplitProject
-
-pause
+echo back to parent dir
+cd ..
+ls -l
+echo
