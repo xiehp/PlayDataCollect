7031cff69924548abf7ea9f001b240ec117eb3e5
xie <xiehp@users.noreply.github.com>
2018-03-29 00:44:43 +0800

    学习

--- script/docker/aliyun/dockerBuild.sh
+++ script/docker/aliyun/dockerBuild.sh
@@ -16,6 +16,7 @@ mvn package -Dmaven.test.skip=true
 
 echo copy $PWD/playdatacollect/app/playdatacollect-collector/target/playdatacollect-collector.jar
 cp $PWD/app/playdatacollect-collector/target/playdatacollect-collector.jar playdatacollect-collector.jar
+cp $PWD/app/playdatacollect-collector/target/playdatacollect-collector.jar script/docker/aliyun/playdata/playdatacollect-collector.jar
 ls -l
 echo
 
