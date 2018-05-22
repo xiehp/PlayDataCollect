ec7d0d61d8885a65286d0500e2dbdbd4743bed5f
xie <xie@qq.com>
2018-05-22 22:04:46 +0800

    学习

--- component/influxdb/src/main/java/xie/playdatacollect/influxdb/action/XInfluxdbAction.java
+++ component/influxdb/src/main/java/xie/playdatacollect/influxdb/action/XInfluxdbAction.java
@@ -7,15 +7,17 @@ import org.influxdb.dto.QueryResult;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import xie.common.utils.log.XLog;
+import xie.common.utils.string.XStringUtils;
 import xie.common.utils.utils.XFormatUtils;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class XInfluxdbAction {
 
 	private InfluxDB influxDB;
-//	@Resource()
+	//	@Resource()
 //	private XInfluxdbProperties influxdbProperties;
 	private Logger logger = XLog.getLog(this.getClass());
 
@@ -28,6 +30,26 @@ public class XInfluxdbAction {
 	public void writePoint(Point point) {
 		influxDB.write(point);
 	}
+
+	/**
+	 * 将SERIES包括index完全移除
+	 * DROP SERIES FROM <measurement_name[,measurement_name]> WHERE <tag_key>='<tag_value>'
+	 *
+	 * @param deleteTagsMap
+	 */
+//	public List<HashMap<String, Object>> queryData(String database, String measurementName, Map<String, String> deleteTagsMap) {
+//		Map<String, Object> map = new LinkedHashMap<>(deleteTagsMap);
+//		String where = XFormatUtils.formatMap2InfluxdbLine(map);
+//		String query ="DROP SERIES from " + measurementName + " where " + where;
+//		logger.info(query);
+//		QueryResult queryResult = influxDB.query(new Query(query, database));
+//		if (queryResult.hasError()) {
+//			logger.error(queryResult.getError());
+//			return false;
+//		}
+//
+//		return true;
+//	}
 //
 //	public void writePoints(List<Point> points) {
 //		BatchPoints batchPoints = BatchPoints.database(influxDB.da).points(points.toArray(new Point[]{})).build();
@@ -43,7 +65,7 @@ public class XInfluxdbAction {
 	public boolean dropSeries(String database, String measurementName, Map<String, String> deleteTagsMap) {
 		Map<String, Object> map = new LinkedHashMap<>(deleteTagsMap);
 		String where = XFormatUtils.formatMap2InfluxdbLine(map);
-		String query ="DROP SERIES from " + measurementName + " where " + where;
+		String query = "DROP SERIES from " + measurementName + " where " + where;
 		logger.info(query);
 		QueryResult queryResult = influxDB.query(new Query(query, database));
 		if (queryResult.hasError()) {
@@ -54,7 +76,37 @@ public class XInfluxdbAction {
 		return true;
 	}
 
-	public void deleteSeries(String database, String measurementName, Map<String, Object> deleteTags, long startTime, long endTime) {
+	/**
+	 * 删除数据, >= startDate, < endDate
+	 *
+	 * @param database database
+	 * @param measurementName measurementName
+	 * @param deleteTagsMap deleteTagsMap
+	 * @param startDate 大于等于该时间
+	 * @param endDate 小于该时间
+	 * @return
+	 */
+	public boolean deleteSeries(String database, String measurementName, Map<String, String> deleteTagsMap, LocalDateTime startDate, LocalDateTime endDate) {
+		Map<String, Object> map = new LinkedHashMap<>(deleteTagsMap);
+		String tagWhereStr = XFormatUtils.formatMap2InfluxdbLine(map);
 
+		String query = "DELETE from " + measurementName + " where 1=1 ";
+		if (XStringUtils.isNotBlank(tagWhereStr)) {
+			query += " and " + tagWhereStr;
+		}
+		if (startDate != null) {
+			query += String.format(" and time >= '%s'", startDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+		}
+//		if (endDate != null) {
+//			query += String.format(" and time < '%s'", endDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+//		}
+		logger.info(query);
+		QueryResult queryResult = influxDB.query(new Query(query, database));
+		if (queryResult.hasError()) {
+			logger.error(queryResult.getError());
+			return false;
+		}
+
+		return true;
 	}
 }
--- component/influxdb/src/test/java/xie/playdatacollect/influxdb/action/XInfluxdbActionTest.java
+++ component/influxdb/src/test/java/xie/playdatacollect/influxdb/action/XInfluxdbActionTest.java
@@ -11,6 +11,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalUnit;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,15 +33,24 @@ public class XInfluxdbActionTest {
 	private InfluxDB influxDB;
 
 	@Test
-	public void test_writePoint() {
+	public void test_writePoint() throws InterruptedException {
 		influxDB.setDatabase("test");
 		influxDB.query(new Query("create database \"test\"", null));
 		Point point1 = Point.measurement("m1").tag("t1", "aaa").addField("f1", 111).build();
-		xInfluxdbAction.writePoint(point1);
+
 		Point point2 = Point.measurement("m1").tag("t1", "bbb").addField("f1", 222).build();
 		Point point3 = Point.measurement("m1").tag("t2", "ccc").addField("f1", 333).build();
+		xInfluxdbAction.writePoint(point1);
 		xInfluxdbAction.writePoint(point2);
 		xInfluxdbAction.writePoint(point3);
+
+//		while (true) {
+			Thread.sleep(2000);
+			xInfluxdbAction.writePoint(point1);
+			xInfluxdbAction.writePoint(point2);
+			xInfluxdbAction.writePoint(point3);
+
+//		}
 	}
 
 
@@ -45,16 +58,25 @@ public class XInfluxdbActionTest {
 	public void test_dropSeries() {
 		Map<String, String> tagMap = new HashMap<>();
 		tagMap.put("t1", "aaa");
-		xInfluxdbAction.dropSeries("test","m1", tagMap);
+		xInfluxdbAction.dropSeries("test", "m1", tagMap);
 
 		tagMap = new HashMap<>();
-		tagMap.put("t1", "bbb");
-		xInfluxdbAction.dropSeries("test","m1", tagMap);
+		tagMap.put("t2", "ccc");
+		xInfluxdbAction.dropSeries("test", "m1", tagMap);
 //		influxDB.query(new Query("DROP SERIES FROM \"m1\" where \"t1\"='aaa'", "test"));
 
 
 		tagMap.put("t1", "");
-		xInfluxdbAction.dropSeries("test","m1", tagMap);
+		xInfluxdbAction.dropSeries("test", "m1", tagMap);
+	}
+
+
+	@Test
+	public void test_deleteSeries() {
+		Map<String, String> tagMap = new HashMap<>();
+		tagMap.put("t1", "aaa");
+		xInfluxdbAction.deleteSeries("test", "m1", tagMap, LocalDate.now().atTime(11, 38), LocalDate.now().atTime(11, 39));
+
 	}
 
 }
