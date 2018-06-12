bb463db1490ff700bfee861d627ff5e5fd0a30c1
xie <xie@qq.com>
2018-06-12 08:01:47 +0800

    学习

--- app/playdatacollect-front/src/main/java/xie/playdatacollect/front/controller/ListController.java
+++ app/playdatacollect-front/src/main/java/xie/playdatacollect/front/controller/ListController.java
@@ -1,9 +1,11 @@
 package xie.playdatacollect.front.controller;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import xie.common.spring.jpa.entity.EntityCache;
+import xie.common.utils.java.XReflectionUtils;
 import xie.framework.core.service.dictionary.dao.AutoQueueDao;
 import xie.framework.core.service.dictionary.entity.AutoQueueEntity;
 import xie.framework.web.controller.BaseController;
@@ -14,7 +16,11 @@ import xie.playdatacollect.influxdb.action.XInfluxdbAction;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.text.Collator;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class ListController extends BaseController {
@@ -34,12 +40,17 @@ public class ListController extends BaseController {
 	@RequestMapping(value = "/list")
 	public String index(Model model, HttpSession session, HttpServletRequest request) {
 
-		List<AutoQueueEntity> list = autoQueueDao.findAll();
-		model.addAttribute("list", list);
+		String classAndMethodStr = XReflectionUtils.getThisClassAndMethodStr();
 
-		List<ProgramEntity> programEntityList = entityCache.get("aaa",
-				() -> programService.findAll()
+		List<ProgramEntity> programEntityList = entityCache.get(classAndMethodStr + ".programEntityList",
+				() -> {
+					Sort sort = new Sort(Sort.Direction.ASC, "fullName");
+					return programService.findAll(sort);
+				}
 		);
+		Collator collator = Collator.getInstance(java.util.Locale.CHINESE);
+		programEntityList.sort((o1, o2) -> collator.compare(o1.getFullName(), o2.getFullName()));
+		model.addAttribute("programEntityList", programEntityList);
 
 		return "list";
 	}
