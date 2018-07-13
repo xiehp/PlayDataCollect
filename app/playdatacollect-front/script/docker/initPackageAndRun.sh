3cec5ef5196ff860bb381af4b01bd2174d54e8e1
xie <xie@qq.com>
2018-07-13 16:16:56 +0800

    学习

---
+++ app/playdatacollect-front/script/docker/initPackageAndRun.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# put this file to a docker deploy folder,
+# and the other script files will first copy to the docker deploy folder
+pwd
+mkdir -p packagePlayDataCollect
+cd packagePlayDataCollect
+pwd
+
+if [ -d "PlayDataCollect" ]; then
+    cd PlayDataCollect
+    git pull
+    git submodule update
+else
+    git clone --recursive ssh://git@gogs.acgimage.cn:53022/xie/PlayDataCollect.git
+    cd PlayDataCollect
+fi
+
+# copy script file
+cp -r packagePlayDataCollect/PlayDataCollect/app/playdatacollect-front/script/docker/* .
+
+
+sh rePackageAndRun.sh
+
+
--- app/playdatacollect-front/script/docker/package.sh
+++ app/playdatacollect-front/script/docker/package.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 
 export MAVEN_OPTS="-Xmx512m"
-
-pwd
-mkdir -p packagePlayDataCollect
-cd packagePlayDataCollect
-pwd
-
-if [ -d "PlayDataCollect" ]; then
-    cd PlayDataCollect
-    git pull
-    git submodule update
-else
-    git clone --recursive ssh://git@gogs.acgimage.cn:53022/xie/PlayDataCollect.git
-    cd PlayDataCollect
-fi
-
-pwd
 mvn clean package -Dmaven.test.skip
 cd ..
 cd ..
